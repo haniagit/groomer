177b72592ee015865e9bbb1cd3fde2aef50ad9a9
Hania <haniagit08@gmail.com>
2018-08-02 16:01:27 +0200

    small stylistic change

--- src/main/java/com/example/groomer/model/Client.java
+++ src/main/java/com/example/groomer/model/Client.java
@@ -8,6 +8,7 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String name;
     private String contact;
     private String dogName;
